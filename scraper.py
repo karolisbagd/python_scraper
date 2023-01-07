93c0cd2095baf8387151366ccd539282051ca070
karolisbagd <karolis.bagd@gmail.com>
2023-01-07 13:34:47 +0000

    Find all

--- scraper.py
+++ scraper.py
@@ -8,3 +8,10 @@ response = requests.get(url)
 # Locate the element
 soup = BeautifulSoup(response.text, "html.parser")
 print(soup.title)
+
+# Find all titles in H2 elements
+blog_titles = soup.find_all('h2', attrs={"class":"blog-card__content-title"})
+for title in blog_titles:
+    print(title.text)
+# Output:
+# Prints all blog tiles on the page
