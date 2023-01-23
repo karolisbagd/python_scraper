042282020569efdad2ceac3fdf445c9e92bc03a1
karolisbagd <karolis.bagd@gmail.com>
2023-01-23 22:48:06 +0000

    Before span

--- scraper.py
+++ scraper.py
@@ -9,13 +9,14 @@ driver = webdriver.Chrome(
 # driver.get("HTML")
 driver.get('https://en.wikipedia.org/wiki/List_of_heads_of_state_of_Lithuania')
 results = []
+other_results = []
 # Add the page source to the variable `content`.
 content = driver.page_source
 # Load the contents of the page, its source, into BeautifulSoup
 # class, which analyzes the HTML as a nested data structure and allows to select
 # its elements by using various selectors.
 soup = BeautifulSoup(content)
-for element in soup.findAll(attrs={'class': 'title'}):
+for element in soup.findAll(attrs={'class': 'mw-headline'}):
     name = element.find('a')
     if name not in results:
         # Add the object of “name” to the list “results” extracts the text in the element, omitting the HTML tags.
@@ -23,6 +24,5 @@ for element in soup.findAll(attrs={'class': 'title'}):
 
 # Turns object into a two-dimensional data table.
 df = pd.DataFrame({'Names': results})
-# 'names.csv' = adding extendsion, index specific starting point in column, encode = specific format
-df.to_csv('names.csv', index=False, encoding='utf-8')
-
+# 'names.csv' = adding extendsion, index = specific starting point in column, encode = specific format
+df.to_csv('names.xlsx', index=False, encoding='utf-8')
