472d702fb33f99b7b97dc40d4e5b2190c2fee7b5
karolisbagd <karolis.bagd@gmail.com>
2023-01-23 20:59:49 +0000

    Before CSV

--- scraper.py
+++ scraper.py
@@ -4,26 +4,18 @@ from selenium import webdriver
 
 
 driver = webdriver.Chrome(
-    executable_path=r'C:\Users\Admin\Documents\UWL\Year3\Project\chromedriver.exe')
+    executable_path=r'C:\Users\Admin\Documents\UWL\Year3\Project\chromedriver.exe')  # webdriver.Chrome()
 
+# driver.get("HTML")
 driver.get('https://en.wikipedia.org/wiki/List_of_heads_of_state_of_Lithuania')
-
 results = []
-
 # Add the page source to the variable `content`.
 content = driver.page_source
 # Load the contents of the page, its source, into BeautifulSoup
 # class, which analyzes the HTML as a nested data structure and allows to select
 # its elements by using various selectors.
 soup = BeautifulSoup(content)
-
-
 for element in soup.findAll(attrs={'class': 'title'}):
     name = element.find('a')
-# Add the object of “name” to the list “results”.
-# `<element>.text` extracts the text in the element, omitting the HTML tags.
+    # Add the object of “name” to the list “results” extracts the text in the element, omitting the HTML tags.
     results.append(name.text)
-
-
-# driver = webdriver.Chrome()
-# driver.get("HTML")
