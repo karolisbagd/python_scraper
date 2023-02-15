d6ef8ea053b4f487867e8bc51f63a2f38771e451
karolisbagd <karolis.bagd@gmail.com>
2023-02-15 22:59:34 +0000

    Changes

--- wikiScrape.py
+++ wikiScrape.py
@@ -3,15 +3,15 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 
 driver = webdriver.Chrome(executable_path=r'C:\path\to\chromedriver.exe')
-driver.get('https://www.gumtree.com/computers-software/w5')
+driver.get('https://en.wikipedia.org/wiki/Solar_System')
 results = []
 content = driver.page_source
 soup = BeautifulSoup(content, 'html.parser')
 
-for element in soup.findAll('h2'):
-    results.append(element.text)
+for header_element in soup.findAll(['h2', 'h3']):
+    results.append(header_element.text)
 
 df = pd.DataFrame({'Text': results})
-df.to_excel('wikipedia_text.xlsx', index=False)
+df.to_csv('wikipedia_text.csv', index=False)
 
 driver.quit()
