2fe205752e6f675bde0c5cb5f03492d45fc4b066
karolisbagd <karolis.bagd@gmail.com>
2023-02-16 18:57:03 +0000

    new changes

--- scraper.py
+++ scraper.py
@@ -2,22 +2,20 @@ import pandas as pd
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
-
 driver = webdriver.Chrome(
     executable_path=r'C:\Users\Admin\Documents\UWL\Year3\Project\chromedriver.exe')  # webdriver.Chrome()
 
 # driver.get("HTML")
 driver.get('https://en.wikipedia.org/wiki/List_of_heads_of_state_of_Lithuania')
 results = []
-other_results = []
 # Add the page source to the variable `content`.
 content = driver.page_source
 # Load the contents of the page, its source, into BeautifulSoup
 # class, which analyzes the HTML as a nested data structure and allows to select
 # its elements by using various selectors.
-soup = BeautifulSoup(content)
-for element in soup.findAll(attrs={'class': 'mw-headline'}):
-    name = element.find('a')
+soup = BeautifulSoup(content, 'html.parser')
+for element in soup.findAll(attrs={'mw-headline': 'house'}):
+    name = element.find('h3')
     if name not in results:
         # Add the object of “name” to the list “results” extracts the text in the element, omitting the HTML tags.
         results.append(name.text)
