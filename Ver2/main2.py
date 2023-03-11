3d445bdbfab5e13ef09c7a27f2e53aafb233a46b
karolisbagd <karolisbagd@gmail.com>
2023-03-11 17:47:56 +0000

    Before INPUT

--- Ver2/main2.py
+++ Ver2/main2.py
@@ -6,18 +6,17 @@ soup = BeautifulSoup(html_text, 'lxml')
 jobs = soup.find_all('article', class_='job search-card')
 
 for job in jobs:
-    company_name = job.find(
-        'a', class_='job__company-link').text.replace(' ', '')
-    description = job.find(
-        'p', class_='job__description noscript-show').text.replace(' ', '')
-    job_title = job.find('h2', class_='job__title').text.replace(
-        ' ', '')  # for span tags use span.text
-
-    print(f'''
-    Company Name: {company_name}
-    Job Title: {job_title}
-    {description}
-    ''')
-
-# print(skills)
-# print(company_name)
+    publish_date = job.find(
+        'p', class_='job__posted-by').text.replace(' ', '')
+    if 'yesterday' in publish_date:
+        company_name = job.find(
+            'a', class_='job__company-link').text.replace(' ', '')
+        description = job.find(
+            'p', class_='job__description noscript-show').text
+        job_title = job.find('h2', class_='job__title').text.replace(
+            ' ', '')  # for span tags use span.text
+        more_info = job.div.h2.a['href']
+        print(f"{publish_date.strip()}")
+        print(f"{description.strip()}")
+        print(f"{more_info}")
+        print(' ')
