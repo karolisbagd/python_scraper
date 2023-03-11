80efc210565a13d01ac7b98fbda6b00f855a8cae
karolisbagd <karolisbagd@gmail.com>
2023-03-11 16:45:26 +0000

    After update

--- Ver2/main2.py
+++ Ver2/main2.py
@@ -3,18 +3,21 @@ import requests
 
 html_text = requests.get('https://www.cv-library.co.uk/cloud-jobs?us=1').text
 soup = BeautifulSoup(html_text, 'lxml')
-job = soup.find('article', class_='job search-card')
-company_name = job.find('a', class_='job__company-link').text.replace(' ', '')
-description = job.find(
-    'p', class_='job__description noscript-show').text.replace(' ', '')
-job_title = job.find('h2', class_='job__title').text.replace(
-    ' ', '')  # for span tags use span.text
+jobs = soup.find_all('article', class_='job search-card')
 
-print(f'''
-Company Name: {company_name}
-Job Title: {job_title}
-{description}
-''')
+for job in jobs:
+    company_name = job.find(
+        'a', class_='job__company-link').text.replace(' ', '')
+    description = job.find(
+        'p', class_='job__description noscript-show').text.replace(' ', '')
+    job_title = job.find('h2', class_='job__title').text.replace(
+        ' ', '')  # for span tags use span.text
+
+    print(f'''
+    Company Name: {company_name}
+    Job Title: {job_title}
+    {description}
+    ''')
 
 # print(skills)
 # print(company_name)
