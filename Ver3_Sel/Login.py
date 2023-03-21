17f7d9397a1a2d669a827d57b9a485f8c19db7ee
karolisbagd <karolis.bagd@gmail.com>
2023-03-21 23:14:04 +0000

    changes

--- Ver3_Sel/Login.py
+++ Ver3_Sel/Login.py
@@ -1,7 +1,8 @@
 import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+import time
+from selenium.webdriver.common.keys import Keys
 
 os.environ['PATH'] += r"C:/SeleniumDrivers"
 driver = webdriver.Chrome()
@@ -10,9 +11,18 @@ driver.get('http://www.automationtesting.co.uk/calculator.html')
 driver.implicitly_wait(5)
 
 first_num = driver.find_element(By.ID, 'result')
-symbol = driver.find_element(By.ID, 'result')
+symbol_add = driver.find_element(By.ID, 'result')
 second_num = driver.find_element(By.ID, 'result')
 
-first_num.send_keys(5)
-symbol.send_keys("+")
-second_num.send_keys(5)
+first_num.send_keys(Keys.NUMPAD5)
+time.sleep(2)
+symbol_add.send_keys(Keys.ADD)
+time.sleep(2)
+second_num.send_keys(Keys.NUMPAD5)
+time.sleep(2)
+ 
+# Passing CSS expression
+btn = driver.find_element(By.CSS_SELECTOR, 'button[onClick="solve()"]')
+
+# classes with spaces are refering to different classes
+# CSS selector is pattern to filter an element by it's styling. 
--- Ver3_Sel/tempCodeRunnerFile.py
+++ Ver3_Sel/tempCodeRunnerFile.py
@@ -1,6 +1,8 @@
 import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
+from selenium.webdriver.common.keys import Keys
 
 os.environ['PATH'] += r"C:/SeleniumDrivers"
 driver = webdriver.Chrome()
@@ -12,6 +14,12 @@ first_num = driver.find_element(By.ID, 'result')
 symbol = driver.find_element(By.ID, 'result')
 second_num = driver.find_element(By.ID, 'result')
 
-first_num.send_keys(5)
-symbol.send_keys("+")
-second_num.send_keys(5)
+first_num.send_keys(Keys.NUMPAD5)
+time.sleep(2)
+symbol.send_keys(Keys.ADD)
+time.sleep(2)
+second_num.send_keys(Keys.NUMPAD5)
+time.sleep(2)
+
+
+# classes with spaces are refering to different classes
