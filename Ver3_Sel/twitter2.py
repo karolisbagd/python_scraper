77a29e0a895ab7a6c2f7373fa926e813028172ea
karolisbagd <karolis.bagd@gmail.com>
2023-03-27 23:10:39 +0100

    final

---
+++ Ver3_Sel/twitter2.py
@@ -0,0 +1,47 @@
+import os
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# Set the path to the chromedriver
+os.environ['PATH'] += r"C:\SeleniumDrivers\chromedriver.exe"
+
+# Create a new instance of the Chrome driver
+driver = webdriver.Chrome()
+
+# Navigate to the Twitter login page
+driver.get("https://twitter.com/login")
+
+# Wait for the page to load
+driver.implicitly_wait(10)
+
+# Find the email input field and enter the email address
+email_field = driver.find_element(By.CLASS_NAME, 'r-30o5oe')
+email_field.send_keys("Student_21464553@outlook.com")
+time.sleep(2)
+
+# Click on the Next button
+next_button = driver.find_element(By.XPATH, '//span[text()="Next"]')
+next_button.click()
+
+# Wait for the password input field to become clickable and enter the password
+driver.implicitly_wait(10)
+time.sleep(2)
+
+# r-1niwhzg, or By.NAME, 'session[password]')
+password_field = driver.find_element(By.NAME, 'password')
+password_field.send_keys('UWL2021!@')
+
+
+# Click on the Log in button
+login_button = driver.find_element(
+    By.XPATH, '//div[@data-testid="LoginForm_Login_Button"]')
+login_button.click()
+
+# Wait for the login process to complete
+time.sleep(2)
+
+# Quit the browser
+driver.quit()
