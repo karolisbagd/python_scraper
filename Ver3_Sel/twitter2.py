9ab7875b10f7296542133e55ff5ad3f97a7e9c9b
karolisbagd <karolis.bagd@gmail.com>
2023-03-28 22:51:27 +0100

    try, except

--- Ver3_Sel/twitter2.py
+++ Ver3_Sel/twitter2.py
@@ -26,21 +26,38 @@ time.sleep(2)
 next_button = driver.find_element(By.XPATH, '//span[text()="Next"]')
 next_button.click()
 
-# Wait for the password input field to become clickable and enter the password
-driver.implicitly_wait(10)
-time.sleep(2)
-
-# r-1niwhzg, or By.NAME, 'session[password]')
-password_field = driver.find_element(By.NAME, 'password')
-password_field.send_keys('UWL2021!@')
-
-
+# Wait for the username or password input field to become clickable and enter the respective value
+try:
+    username_field = driver.find_element(By.NAME, 'text')
+    username_field.send_keys('S2146455365372')
+    time.sleep(2)
+    next_button = driver.find_element(By.XPATH, '//span[text()="Next"]')
+    next_button.click()
+    time.sleep(2)
+    
+    # Wait for the password input field to become clickable and enter the password
+    password_field = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, 'password')))
+    password_field.send_keys('UWL2021!@')
+    
+except:
+    try:
+        # Wait for the password input field to become clickable and enter the password
+        password_field = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, 'password')))
+        password_field.send_keys('UWL2021!@')
+        
+    except:
+        print("Could not find password field.")
+    
 # Click on the Log in button
-login_button = driver.find_element(
-    By.XPATH, '//div[@data-testid="LoginForm_Login_Button"]')
+login_button = driver.find_element(By.XPATH, '//div[@data-testid="LoginForm_Login_Button"]')
 login_button.click()
 
 # Wait for the login process to complete
+time.sleep(2)
+
+# Click on Profile
+profile_button = driver.find_element(By.CSS_SELECTOR, 'a[aria-label="Profile"]')
+profile_button.click()
 time.sleep(2)
 
 # Quit the browser
