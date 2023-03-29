49c0272b821097c364dbee30024aa8cef21d2b38
karolisbagd <karolis.bagd@gmail.com>
2023-03-29 15:59:24 +0100

    mod

--- Ver3_Sel/twitter2.py
+++ Ver3_Sel/twitter2.py
@@ -34,31 +34,45 @@ try:
     next_button = driver.find_element(By.XPATH, '//span[text()="Next"]')
     next_button.click()
     time.sleep(2)
-    
+
     # Wait for the password input field to become clickable and enter the password
-    password_field = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, 'password')))
+    password_field = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.NAME, 'password')))
     password_field.send_keys('UWL2021!@')
-    
+
 except:
     try:
         # Wait for the password input field to become clickable and enter the password
-        password_field = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, 'password')))
+        password_field = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.NAME, 'password')))
         password_field.send_keys('UWL2021!@')
-        
+
     except:
         print("Could not find password field.")
-    
+
 # Click on the Log in button
-login_button = driver.find_element(By.XPATH, '//div[@data-testid="LoginForm_Login_Button"]')
+login_button = driver.find_element(
+    By.XPATH, '//div[@data-testid="LoginForm_Login_Button"]')
 login_button.click()
 
 # Wait for the login process to complete
 time.sleep(2)
 
 # Click on Profile
-profile_button = driver.find_element(By.CSS_SELECTOR, 'a[aria-label="Profile"]')
+profile_button = driver.find_element(
+    By.CSS_SELECTOR, 'a[aria-label="Profile"]')
 profile_button.click()
 time.sleep(2)
 
+
+scrape_results = driver.find_element( By.ID, ''
+    ).find_element(By.CLASS_NAME, 'css-1dbjc4n')
+print(scrape_results)
+    
+'''for tweet in tweets:
+    find_elements By. ID, class,
+    Loop through class
+    Extract and store data.
+'''
 # Quit the browser
 driver.quit()
