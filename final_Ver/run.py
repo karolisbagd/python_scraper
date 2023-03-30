be00f9be80edaa874f01a49379d11a42b115564f
karolisbagd <karolis.bagd@gmail.com>
2023-03-30 19:56:08 +0100

    run class

---
+++ final_Ver/run.py
@@ -0,0 +1,9 @@
+from main import Twitter
+
+with Twitter() as bot:
+    bot.load_page()
+    bot.input_email()
+    bot.input_password()
+    bot.button_login()
+    bot.click_profile()
+    #print(len(bot.tweet_results()))  # Need to check Console?
