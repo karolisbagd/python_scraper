447d3fab2a7860237c35f807cf4280cf8e3cc847
karolisbagd <karolis.bagd@gmail.com>
2023-04-21 23:43:25 +0100

    com

--- final_Ver/tempCodeRunnerFile.py
+++ final_Ver/tempCodeRunnerFile.py
@@ -1,10 +1,27 @@
-from main import SocialMedia
+    post_texts = []
 
-with SocialMedia() as bot:
-    bot.load_page()
-    bot.twitter_input_email()
-    bot.input_password()
-    bot.twitter_button_login()
-    bot.twitter_click_profile()
-    print(bot.tweet_results())
-    # print(len(bot.tweet_results()))  # Need to check Console?
+        # Scroll down the webpage
+        self.execute_script("window.scrollBy(0, 700);")
+        self.implicitly_wait(5)
+
+        # Locate all the post elements on the page
+        post_elements = self.find_elements(
+            By.XPATH, ".//div[@data-pagelet='ProfileTimeline']")
+
+        print("Scraping post from the timeline")
+        time.sleep(2)
+
+        print(f"Number of post elements: {len(post_elements)}")
+
+        for post_element in post_elements:
+            post_text_element = post_element.find_element(
+                By.XPATH, ".//div[@class='x11i5rnm xat24cr x1mh8g0r x1vvkbs xdj266r']").text
+            self.implicitly_wait(5)
+            time_element = post_element.find_element(
+                By.XPATH, ".//a[@role='link']/span").text
+            self.implicitly_wait(5)
+
+            post_texts.append(
+                {"Text": post_text_element, "Posted": time_element})
+
+        return post_texts  # Return the text of all the posts
