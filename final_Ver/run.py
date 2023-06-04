254620c6e6c5b2d997e020fb3f4e122fa549cf6c
karolisbagd <karolis.bagd@gmail.com>
2023-06-04 13:38:39 +0100

    minor changes

--- final_Ver/run.py
+++ final_Ver/run.py
@@ -1,35 +1,20 @@
-# from multiprocessing import process
-from main import SocialMedia
-#import psutil
+from Main import SocialMedia
 
 with SocialMedia() as bot:
 
-
-
    bot.open_html_file("w")  # Open the HTML file in write mode
 
-   # bot.twitter_load_page()
-   # bot.twitter_input_email()
-   # bot.twitter_input_password()
-   # bot.twitter_button_login()
-   # bot.twitter_click_profile()
-   # bot.tweet_results()
-
-   # bot.end_html_file()  # Close the HTML file after writing all the tweets
+   bot.twitter_load_page()
+   bot.twitter_input_email()
+   bot.twitter_input_password()
+   bot.twitter_button_login()
+   bot.twitter_click_profile()
+   bot.tweet_results()
+   bot.end_html_file()  # Close the HTML file after writing all the tweets
 
    bot.facebook_load_page()
    bot.facebook_login_page()
    bot.facebook_click_profile()
-
    bot.open_html_file("a")  # Open the HTML file in append mode
-
    bot.scrape_posts()
-  
    bot.end_html_file()  # Close the HTML file after writing all the posts
-
-   # Get CPU and memory usage
-   #cpu_percent = process.cpu_percent()                               # type: ignore
-   #memory_usage = process.memory_info().rss / 1024 / 1024 # in MB           # type: ignore # in MB        
-    
-   #print(f"CPU usage: {cpu_percent}%")
-   #print(f"Memory usage: {memory_usage} MB")
